eee229ea612c7dc3d792acfcdf15c3fa1b348986
PRITHVI <biju.ganesan@gmail.com>
2019-12-22 11:20:56 +0530

    added log

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -32,8 +33,8 @@ function setup(){
 
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
+    log5 = new Log(870,120,150, PI/7);
+    log6 = new log(230,180,80,PI/2);
     bird = new Bird(100,100);
 
 }
